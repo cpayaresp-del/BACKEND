c45720d80d284b6f0ab298db7a795bb6bab9b7f1
cpayaresp-del <cpayaresp@unicartagena.edu.co>
2026-05-12 13:01:02 -0500

    Add files via upload

---
+++ Backend/src/models/Review.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: false, // Opcional
+    },
+  },
+  { timestamps: true }
+);
+
+// Índice único para evitar múltiples reseñas por usuario y producto
+reviewSchema.index({ user: 1, product: 1 }, { unique: true });
+
+module.exports = mongoose.model('Review', reviewSchema);
