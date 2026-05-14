b76bf50d6bbf853fe9c7ef7e8400126fa232d2e9
cpayaresp-del <cpayaresp@unicartagena.edu.co>
2026-05-13 21:41:24 -0500

    Update productRoutes.js

--- Backend/src/routes/productRoutes.js
+++ Backend/src/routes/productRoutes.js
@@ -2,17 +2,19 @@ const express = require('express');
 const {
   getProducts,
   getProduct,
+  getSimilarProducts,
   createProduct,
   updateProduct,
   deleteProduct,
 } = require('../controllers/productController');
 
 const { protect } = require('../middlewares/authMiddleware');
-const authorize = require('../middlewares/roleMiddleware'); // ✅ CORREGIDO
+const authorize = require('../middlewares/roleMiddleware');
 
 const router = express.Router();
 
 router.get('/', getProducts);
+router.get('/:id/similar', getSimilarProducts);
 router.get('/:id', getProduct);
 
 router.post('/', protect, authorize('admin'), createProduct);
